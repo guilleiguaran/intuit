9ca43fd939c7f6a58320265a81e750ef27b7a952
Eugene Bolshakov <eugene.bolshakov@gmail.com>
2011-11-25 12:16:40 +0400

    Config

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+require "intuit"
+require "active_support/test_case"
+require "mocha"
+
+class ActiveSupport::TestCase
+  include Mocha::API
+end
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
