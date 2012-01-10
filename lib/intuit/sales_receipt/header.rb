670e9e2a952964d08d1e31551f2db7ef6f99311d
Eugene Bolshakov <eugene.bolshakov@gmail.com>
2012-01-10 08:44:07 +0400

    Added SalesReceipt#header#tax_name

--- lib/intuit/sales_receipt/header.rb
+++ lib/intuit/sales_receipt/header.rb
@@ -6,6 +6,7 @@ module Intuit
       element "CustomerId",     :as => :customer_id, :class => Id
       element "CustomerName",   :as => :customer_name
       element "TaxId",          :as => :tax_id, :class => Id
+      element "TaxName",        :as => :tax_name
       element "ShipDate",       :as => :ship_date
       element "ShipAddr",       :as => :shipping_address, :class => Address
     end
